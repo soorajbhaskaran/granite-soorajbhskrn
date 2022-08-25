4b7fbf9036fd3c79ab0a858c178cca0ef2b2104e
Sooraj Bhaskaran <johndoe@example.com>
2022-08-25 22:31:33 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import { setAuthHeaders } from "apis/axios";
+
 import { initializeLogger } from "./common/logger";
 
 const App = () => {
-const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     initializeLogger();
@@ -14,6 +17,7 @@ const [loading, setLoading] = useState(true);
   if (loading) {
     return <h1>Loading...</h1>;
   }
+
   return (
     <Router>
       <Switch>
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
-class Task <ApplicationRecord
-    MAX_TITLE_LENGTH=125
-    validates :title , presence: true , length: { maximum: MAX_TITLE_LENGTH }
-end   
+# frozen_string_literal: true
+
+class Task < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+end
