1a5062fe6986cfd6595352169a134444304b76a7
Sooraj Bhaskaran <johndoe@example.com>
2022-08-29 14:19:46 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,35 +1,37 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import { setAuthHeaders } from "apis/axios";
-
-import { initializeLogger } from "./common/logger";
-import Dashboard from "./components/Dashboard";
-import PageLoader from "./components/PageLoader";
-import CreateTask from "./components/Tasks/Create";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
+import CreateTask from "components/Tasks/Create";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     initializeLogger();
+    registerIntercepts();
     setAuthHeaders(setLoading);
   }, []);
 
   if (loading) {
-    <div className="h-screen">
-      <PageLoader />
-    </div>;
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
   }
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={CreateTask} path="/tasks/create" />
+        <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
     </Router>
   );
--- config/webpack/alias.js
+++ config/webpack/alias.js
@@ -1,10 +1,11 @@
 module.exports = {
-    resolve: {
-      alias: {
-        apis: "src/apis",
-        common: "src/common",
-        components: "src/components",
-      },
+  resolve: {
+    alias: {
+      apis: "src/apis",
+      common: "src/common",
+      components: "src/components",
+      utils: "src/utils",
+      constants: "src/constants",
     },
-  };
-  
+  },
+};
