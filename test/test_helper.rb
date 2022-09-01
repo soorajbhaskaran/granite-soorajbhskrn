4c91a790b1adf9b75836d84097a159f0d6b619e9
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-09-01 23:07:34 +0530

    Integrated factory-bot and faker

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,6 +23,7 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   include ActionView::Helpers::TranslationHelper
+  include FactoryBot::Syntax::Methods
 
   # Run tests in parallel with specified workers
   # Run tests in parallel with specified workers
